83c11a6a7b6c1e055f71cb2ecc7fdc498965f852
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-01-03 20:30:07 +1000

    📈 added vercel analytics (#82)
    * added vercel analytics
    
    * only use vercel analytics in PROD

--- components/analytics/index.tsx
+++ components/analytics/index.tsx
@@ -4,6 +4,7 @@ import Plausible from './Plausible'
 import SimpleAnalytics from './SimpleAnalytics'
 import Umami from './Umami'
 import siteMetadata from '@/data/siteMetadata'
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 
 declare global {
   interface Window {
@@ -22,6 +23,7 @@ const Analytics = () => {
       {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
       {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
       {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
+      {isProduction && <VercelAnalytics />}
     </>
   )
 }
