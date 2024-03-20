e2cd27ff2d69c9db07e4f1c9cbd7fe887c0025aa
Daniel Mackay <2636640+danielmackay@users.noreply.github.com>
2024-03-20 22:02:32 +1000

    Fix lint issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,41 +59,41 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-      <html
-          lang={siteMetadata.language}
-          className={`${space_grotesk.variable} scroll-smooth`}
-          suppressHydrationWarning
-      >
+    <html
+      lang={siteMetadata.language}
+      className={`${space_grotesk.variable} scroll-smooth`}
+      suppressHydrationWarning
+    >
       {/*<link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png"/>*/}
       {/*<link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png"/>*/}
       {/*<link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png"/>*/}
       {/*<link rel="manifest" href="/static/favicons/site.webmanifest"/>*/}
       {/*<link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5"/>*/}
       {/*<meta name="msapplication-TileColor" content="#000000"/>*/}
-      <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png"/>
-      <link rel="manifest" href="/static/favicons/site.webmanifest"/>
-      <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#030712"/>
-      <meta name="msapplication-TileColor" content="#030712"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+      <link rel="manifest" href="/static/favicons/site.webmanifest" />
+      <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#030712" />
+      <meta name="msapplication-TileColor" content="#030712" />
 
-      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff"/>
-      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#030712"/>
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml"/>
+      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
+      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#030712" />
+      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
-      <ThemeProviders>
-        <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig}/>
-        <SectionContainer>
-          <div className="flex h-screen flex-col justify-between font-sans">
-            <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
-              <Header/>
-              <main className="mb-auto">{children}</main>
-            </SearchProvider>
-            <Footer/>
-          </div>
-        </SectionContainer>
-      </ThemeProviders>
+        <ThemeProviders>
+          <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
+          <SectionContainer>
+            <div className="flex h-screen flex-col justify-between font-sans">
+              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+                <Header />
+                <main className="mb-auto">{children}</main>
+              </SearchProvider>
+              <Footer />
+            </div>
+          </SectionContainer>
+        </ThemeProviders>
       </body>
-      </html>
+    </html>
   )
 }
--- components/ThemeImage.tsx
+++ components/ThemeImage.tsx
@@ -3,24 +3,24 @@ import { useTheme } from 'next-themes'
 import Image, { ImageProps } from 'next/image'
 import { useEffect, useState } from 'react'
 type Props = Omit<ImageProps, 'src' | 'priority' | 'loading'> & {
-    srcLight: string
-    srcDark: string
+  srcLight: string
+  srcDark: string
 }
 const ThemeImage = (props: Props) => {
-    const [showLight, setShowLight] = useState(false)
-    const { theme } = useTheme()
-    const { srcLight, srcDark, ...rest } = props
+  const [showLight, setShowLight] = useState(false)
+  const { theme } = useTheme()
+  const { srcLight, srcDark, ...rest } = props
 
-    useEffect(() => {
-        setShowLight(theme === 'dark')
-    }, [theme])
+  useEffect(() => {
+    setShowLight(theme === 'dark')
+  }, [theme])
 
-    return (
-        <>
-            <Image {...rest} src={srcLight} className={`${showLight ? 'hidden' : ''}`} />
-            <Image {...rest} src={srcDark} className={`${showLight ? '' : 'hidden'}`} />
-        </>
-    )
+  return (
+    <>
+      <Image {...rest} src={srcLight} className={`${showLight ? 'hidden' : ''}`} />
+      <Image {...rest} src={srcDark} className={`${showLight ? '' : 'hidden'}`} />
+    </>
+  )
 }
 
 export default ThemeImage
