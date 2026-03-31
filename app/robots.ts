9673d01929e79b9110ff5a29c2eec4753e3f76e3
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2026-03-31 18:26:44 +1000

    fix(robots): update GPTBot rules to allow access to /feed.xml

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: 'GPTBot',
         disallow: '/',
+        allow: '/feed.xml',
       },
       {
         userAgent: '*',
